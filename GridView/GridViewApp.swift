849dd961def21e5921835b3ef2693a6ee26cf248
armaan535kh <kh.armaan535@gmail.com>
2024-09-10 07:06:08 +0530

    Initial Commit

---
+++ GridView/GridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewApp.swift
+//  GridView
+//
+//  Created by Armaan Khan  on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
